27975cc5192ef62a7ae10aeca8105c20a2b75dd9
bhojaraja <nsbraj@yahoo.com>
2024-01-17 10:30:39 +0000

    second commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello
 public static void main(String str[])
 {
 System.out.println("Dear All, Welcome to Jenkins Demo");
+System.out.println("Adding new message to understand schedule tasks");
 }
 }
